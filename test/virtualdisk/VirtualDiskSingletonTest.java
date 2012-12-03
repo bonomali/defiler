fa96a8e9cd1bf0027787f63afc3fcd59290d6619
Kevin Gao <sudowork@gmail.com>
2012-12-03 09:40:12 -0500

    Removed deprecated test

--- test/virtualdisk/VirtualDiskSingletonTest.java
+++ test/virtualdisk/VirtualDiskSingletonTest.java
@@ -1,10 +1,5 @@
 package virtualdisk;
 
-import java.io.IOException;
-
-import common.Constants;
-
-import dblockcache.DBuffer;
 import junit.framework.TestCase;
 
 public class VirtualDiskSingletonTest extends TestCase {
@@ -13,23 +8,5 @@ public class VirtualDiskSingletonTest extends TestCase {
 		VirtualDisk vd = VirtualDiskSingleton.getInstance(true);
 		assertNotNull(vd);
 	}
-	
-	/*public void testReadWrite() throws IllegalArgumentException, IOException {
-		VirtualDisk vd = VirtualDiskSingleton.getInstance();
-		DBuffer db = new DBuffer(0);
-		DBuffer db2 = new DBuffer(1);
-		byte[] buf = {0x0, 0x1, 0x2, 0x3};
-		db.write(buf, 0, 5);
-		db2.write(buf, 0, 5);
-		vd.startRequest(db, Constants.DiskOperationType.WRITE);
-		vd.startRequest(db2, Constants.DiskOperationType.WRITE);
-		
-		for (int i = 0; i < buf.length; i++) buf[i] = 0x0;
-		db2.write(buf, 0, 5);
-		assertEquals(0x0, db2.getBuffer()[buf.length - 1]);
-		vd.startRequest(db2, Constants.DiskOperationType.READ);
-		assertEquals(0x3, db2.getBuffer()[buf.length - 1]);
-	}*/
-	
 
 }
